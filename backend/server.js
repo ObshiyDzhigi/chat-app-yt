ea49f45e0c939fbb29dcd4d4634da868a11b4482
zhigazh2017@outlook.com <zhigazh2017@outlook.com>
2024-05-12 20:03:03 +0500

    change

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from "express"
+import path from "path"
 import dotenv from 'dotenv'
 import cookieParser from 'cookie-parser'
 
@@ -9,6 +10,8 @@ import connectToMongoDB from "./db/connectToMongoDB.js";
 import { app,server,io } from "../frontend/src/socket/socket.js"
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve()
+
 dotenv.config();
 
 app.use(express.json())
@@ -18,6 +21,11 @@ app.use("/api/auth",authRoutes)
 app.use("/api/messages",messageRoutes)
 app.use("/api/users",userRoutes)
 
+app.use(express.static(path.join(__dirname,"/frontend/dist")))
+
+app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname,"frontend","dist","index.html"),)
+})
 // app.get("/",(req,res)=>{
 //     // root route https://localhost:5000/
 //     res.send("Hello World!")
